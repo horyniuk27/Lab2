cbcce515d15408c44d14258a8b689173d4fb7597
Dima <horyniuk.dmytro@chnu.edu.ua>
2024-04-08 16:30:34 +0300

    Budget h file

--- BudgetTracker.h
+++ BudgetTracker.h
@@ -9,27 +9,15 @@ class BudgetTracker {
 private:
     std::vector<Expense> expenses;
     std::vector<Income> incomes;
+    static int instances;
 
 public:
-    // Конструктор за замовчуванням
     BudgetTracker();
-
-    // Деструктор
     ~BudgetTracker();
-
-    // Метод для додавання витрати
     void addExpense(const Expense& exp);
-
-    // Метод для додавання доходу
     void addIncome(const Income& inc);
-
-    // Метод для виведення витрат
     void printExpenses() const;
-
-    // Метод для виведення доходів
     void printIncomes() const;
-
-    // Статичний метод для отримання кількості створених об'єктів
     static int getInstances();
 };
 
