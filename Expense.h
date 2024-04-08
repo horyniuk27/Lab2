8859a8138c884cc15d9e781152dadf61756beea8
Dima <horyniuk.dmytro@chnu.edu.ua>
2024-04-08 16:31:52 +0300

    Expense h file

--- Expense.h
+++ Expense.h
@@ -1,3 +1,4 @@
+// Expense.h
 #ifndef EXPENSE_H
 #define EXPENSE_H
 
@@ -10,30 +11,15 @@ private:
     std::string description;
 
 public:
-    // Конструктор
     Expense(float amt, const std::string& cat, const std::string& desc);
-
-    // Перевантажений конструктор
     Expense();
-
-    // Деструктор
     ~Expense();
-
-    // Методи доступу до полів класу
     float getAmount() const;
     std::string getCategory() const;
     std::string getDescription() const;
-
-    // Унарний оператор "-"
     Expense operator-() const;
-
-    // Бінарний оператор "+"
     Expense operator+(const Expense& other) const;
-
-    // Дружня функція для перевантаження оператора виводу в потік
     friend std::ostream& operator<<(std::ostream& os, const Expense& exp);
-
-    // Дружня функція для перевантаження оператора вводу з потоку
     friend std::istream& operator>>(std::istream& is, Expense& exp);
 };
 
